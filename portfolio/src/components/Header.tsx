cecdb38b39f78f91e841f05b50564446b1ee654d
deokbin <deokbin@naver.com>
2024-09-27 00:48:50 +0900

    feat: 헤더 홈 네비게이션 추가

--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,11 +1,16 @@
 import "./Header.css";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const navigate = useNavigate();
+  const navToHome = () => navigate("/");
+  const navToAboutMe = () => navigate("/about-me");
   return (
     <div className="header">
       <h2>DB's Portfolio</h2>
       <nav className="nav-bar">
-        <a href="#about-me">About Me</a>
+        <a onClick={navToHome}>Home</a>
+        <a onClick={navToAboutMe}>About Me</a>
         <a href="#projects">Projects</a>
         <a href="#contact">Contact</a>
       </nav>
