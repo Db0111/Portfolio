1a6a655bc3c6edbd5c062961139df4c117b9d250
deokbin <deokbin@naver.com>
2024-09-27 00:47:04 +0900

    feat: 라우트 경로 설정

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./pages/main";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <>
-      <Header />
-      <Main />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/about-me" element={<AboutMe />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
