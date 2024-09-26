fded560ca89bd31b80c55b4b48e5b85810c79171
deokbin <deokbin@naver.com>
2024-09-26 11:05:11 +0900

    메인 페이지 디자인 1차

---
+++ portfolio/src/pages/main.tsx
@@ -0,0 +1,13 @@
+import "animate.css";
+import "./Main.css";
+
+const Main = () => {
+  return (
+    <div className="main_container">
+      <h1 className="animate__animated animate__flipInX">Kim</h1>
+      <h1 className="animate__animated animate__flipInX">Deokbin</h1>
+      <h2 className="animate__animated animate__flipInX">Frontend-Developer</h2>
+    </div>
+  );
+};
+export default Main;
