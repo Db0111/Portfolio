a55e759b00391a4522a2b0732760d9c53f59a109
deokbin <deokbin@naver.com>
2025-08-05 18:03:31 +0900

    feat: 다크 모드 기능 적용

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -5,12 +5,28 @@ import Main from "./pages/MainPage";
 import AboutMe from "./pages/AboutMe";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(
+    window.matchMedia("(prefers-color-scheme: dark)").matches
+  );
+
+  useEffect(() => {
+    const root = document.documentElement;
+    if (isDarkMode) {
+      root.classList.add("dark");
+    } else {
+      root.classList.remove("dark");
+    }
+  }, [isDarkMode]);
   return (
     <>
       <BrowserRouter>
-        <Header />
+        <Header
+          isDarkMode={isDarkMode}
+          toggleDarkMode={() => setIsDarkMode((prev) => !prev)}
+        />
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/about-me" element={<AboutMe />} />
--- portfolio/src/components/Header.tsx
+++ portfolio/src/components/Header.tsx
@@ -1,22 +1,32 @@
+import { Moon, Sun } from "lucide-react";
 import "./Header.css";
 import { useNavigate } from "react-router-dom";
 
-const Header = () => {
+const Header = ({
+  isDarkMode,
+  toggleDarkMode,
+}: {
+  isDarkMode: boolean;
+  toggleDarkMode: () => void;
+}) => {
   const navigate = useNavigate();
   const navToHome = () => navigate("/");
   const navToAboutMe = () => navigate("/about-me");
   const navToProjects = () => navigate("/projects");
   const navToContact = () => navigate("/contact");
   return (
-    <div className="header">
+    <header className="header">
       <h2>DB's Portfolio</h2>
       <nav className="nav-bar">
         <a onClick={navToHome}>Home</a>
         <a onClick={navToAboutMe}>About Me</a>
         <a onClick={navToProjects}>Projects</a>
         <a onClick={navToContact}>Contact</a>
+        <button onClick={toggleDarkMode}>
+          {isDarkMode ? <Sun /> : <Moon />}
+        </button>
       </nav>
-    </div>
+    </header>
   );
 };
 
